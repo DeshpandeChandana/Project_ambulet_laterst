b3820e98140c9936870776bcd80b4670f2242388
Chandana S Deshpande <DeshpandeChandana@users.noreply.github.com>
2016-08-25 11:37:12 +0530

    Update simulate.sh

--- Ambulet_Changes-master/src/seamo_prehandoff/simulate.sh
+++ Ambulet_Changes-master/src/seamo_prehandoff/simulate.sh
@@ -1,5 +1,5 @@
- sh -x threeg_bw/wget_bw.sh ppp1 docomo dtemp&
+ sh -x threeg_bw/wget_bw.sh ppp2 docomo dtemp&
 	
- sh -x threeg_bw/wget_bw.sh ppp0 bsnl btemp &
+ sh -x threeg_bw/wget_bw.sh ppp1 Idea btemp &
 
- sh -x threeg_bw/wget_bw.sh ppp2 airtel atemp 
+ sh -x threeg_bw/wget_bw.sh ppp0 airtel atemp 
